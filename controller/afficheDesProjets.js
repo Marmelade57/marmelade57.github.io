a254f704c023dbfee04c6a41c16e3be37173a05a
Marmelade571 <margaux.haller2@etu.univ-lorraine.fr>
2025-11-11 13:26:19 +0100

    Init projet

---
+++ controller/afficheDesProjets.js
@@ -0,0 +1,12 @@
+import { donnees } from "../model/projets.json";
+
+console.info(donnees)
+
+let section = document.querySelector("section");
+let article = document.createElement("article");
+
+for (const donnee of donnees) {
+    console.info(donnee);
+    article.innerHTML = "<h1>"+donnee.titre+"</h1><small>";+donnee.lieu+"</small><p>"+donnee.date+"</p>";
+    section.appendChild(article);
+}
