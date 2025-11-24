b04fd2da176c0b96a6dac7e1fd2c656049f17dfc
Marmelade571 <margaux.haller2@etu.univ-lorraine.fr>
2025-11-24 10:47:31 +0100

    ajout footer

---
+++ controller/footer.js
@@ -0,0 +1,5 @@
+let footer = document.createElement("footer");
+footer.style = "border-top: 1px solid black; margin:auto; margin-top:16px;"
+footer.innerHTML = "<p>Ce site est actuellement en construction. Tous les boutons 'En savoir plus' ne mènent à aucune information concrète pour l'instant.</p><p>À ma connaissance, ce site ne collecte pas de cookies.</p>";
+
+document.body.append(footer);
